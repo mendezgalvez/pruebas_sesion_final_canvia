72e7edfcb9451ef751d001922a6ad774e85caf69
mendezgalvez <37353798+mendezgalvez@users.noreply.github.com>
2021-05-31 07:17:24 -0500

    Update reto_final_caso_arbitrario.py
    commit 4

--- pruebas_sesion_final/reto_final_caso_arbitrario.py
+++ pruebas_sesion_final/reto_final_caso_arbitrario.py
@@ -17,26 +17,30 @@ class TestRetoFinalCaso(unittest.TestCase):
 
     def tearDown(self) -> None:
         self.driver.close()
+
+    @unittest.skip("test")
     def test_reto_final_caso_arbitrario1(self):
         driver = self.driver
         driver.get("https://demoqa.com/buttons")
         submit_button = driver.find_element_by_xpath("//button[@id='rightClickBtn']")
         submit_button.click()
-        time.sleep(5)
+        time.sleep(2)
 
+    @unittest.skip("test")
     def test_reto_final_caso_arbitrario2(self):
         driver = self.driver
         driver.get("https://demoqa.com/dynamic-properties")
         submit_button = driver.find_element_by_xpath("//button[@id='colorChange']")
         submit_button.click()
-        time.sleep(5)
-        
+        time.sleep(2)
+
     @unittest.skip("test")
     def test_reto_final_caso_arbitrario3(self):
         driver = self.driver
         driver.get("https://demoqa.com/webtables")
         submit_button_1 = driver.find_element_by_xpath("//button[@id='addNewRecordButton']")
         submit_button_1.click()
+        time.sleep(3)
         firstName = driver.find_element_by_xpath("//input[@id='firstName']")
         lastName = driver.find_element_by_xpath("//input[@id='lastName']")
         userEmail = driver.find_element_by_xpath("//input[@id='userEmail']")
@@ -50,21 +54,26 @@ class TestRetoFinalCaso(unittest.TestCase):
         salary.send_keys("1000")
         department.send_keys("DS")
         submit_button_2 = driver.find_element_by_xpath("//button[@id='submit']")
+        time.sleep(3)
         submit_button_2.click()
 
-    @unittest.skip("test")
     def test_reto_final_caso_arbitrario4(self):
         driver = self.driver
-        driver.get("https://demoqa.com/alerts")
-        submit_button_alert = driver.find_element_by_xpath("//button[@id='alertButton']")
-        submit_button_alert.click()
-        submit_button_timer = driver.find_element_by_xpath("//button[@id='timerAlertButton']")
-        submit_button_timer.click()
-        submit_button_confirm = driver.find_element_by_xpath("//button[@id='confirmButton']")
-        submit_button_confirm.click()
-        submit_button_prompt = driver.find_element_by_xpath("//button[@id='promtButton']")
-        submit_button_prompt.click()
-        time.sleep(5)
+        driver.get("https://demoqa.com/modal-dialogs")
+        submit_button_small= driver.find_element_by_xpath("//button[@id='showSmallModal']")
+        time.sleep(2)
+        submit_button_small.click()
+        time.sleep(2)
+        submit_button_closem = driver.find_element_by_xpath("//button[@id='closeSmallModal']")
+        submit_button_closem.click()
+
+        time.sleep(2)
+        submit_button_large = driver.find_element_by_xpath("//button[@id='showLargeModal']")
+        time.sleep(2)
+        submit_button_large.click()
+        time.sleep(2)
+        submit_button_closel = driver.find_element_by_xpath("//button[@id='closeLargeModal']")
+        submit_button_closel.click()
 
     @unittest.skip("test")
     def test_reto_final_caso_arbitrario5(self):
@@ -72,13 +81,15 @@ class TestRetoFinalCaso(unittest.TestCase):
         driver.get("https://demoqa.com/browser-windows")
         submit_button_tab = driver.find_element_by_xpath("//button[@id='tabButton']")
         submit_button_tab.click()
+        time.sleep(2)
+
         submit_button_window = driver.find_element_by_xpath("//button[@id='windowButton']")
         submit_button_window.click()
+        time.sleep(2)
+
         submit_button_message = driver.find_element_by_xpath("//button[@id='messageWindowButton']")
         submit_button_message.click()
-        time.sleep(5)
-
-
+        time.sleep(2)
 
 if __name__ == '__main__':
     unittest.main()
